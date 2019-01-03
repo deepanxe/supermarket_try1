6b6e9c15fcae84cb1ba9d27eb56a41ce2056ba26
Deepan Raj <deepan.xe@gmail.com>
2019-01-03 14:39:07 +0530

    First Creation

---
+++ app/src/main/java/super_market/supermarket/MainActivity.java
@@ -0,0 +1,14 @@
+package super_market.supermarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_activity
+        );
+    }
+}
